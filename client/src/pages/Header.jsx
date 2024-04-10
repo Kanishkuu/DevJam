049c1b059c1bc8279a6a274ac67348122f93b41f
Research Laptop <researchlaptop@research_.mcbs.edu.om>
2024-04-11 02:11:26 +0530

    changed logo

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -7,18 +7,18 @@ export default function
     
         <header className=' flex justify-between'>
       <a href="" className="flex items-center gap-1">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w- h-4 -rotate-90">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 -rotate-90">
            <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
       </svg>
-      <span className='font-bold text-xl'>AirBnc</span>
+      <span className='font-bold text-xl'>Sleepy Shack</span>
       </a>
 
       <div className='flex gap-2 border border -gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'>
-        <div>anywhere</div>
+        <div>Anywhere</div>
         <div className=" border-l border-gray-300"></div>
-        <div>any week</div> 
+        <div>Any Week</div>
         <div className=" border-l border-gray-300"></div>
-        <div>Add guest</div>
+        <div>Add Guest</div>
         <button className='bg-primary text-white p-1 rounded-full'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
